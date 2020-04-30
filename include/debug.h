92d284fe2ed28ae2265cb3cd6f4db952aa043ab4
Aaron Robert <Robert.Aaron2017@outlook.com>
2020-04-30 21:26:25 +0800

    feat(debug): add ERR macro

--- include/debug.h
+++ include/debug.h
@@ -1,9 +1,20 @@
 #pragma once
-#include <iostream>
 #define DEBUG
 #ifdef DEBUG
+#  include <iostream>
 #  define LOG_VALUE(value) #  value << "=" << value << " "
 #  define LOG_NV(name, value) name << "=" << value << " "
 #  define DLOG(exp) \
-    std::cout << __FILE__ << ":" << __LINE__ << "(" << errno << "):" exp << std::endl;
+    std::cout << __FILE__ << ":" << __LINE__ << "(" << errno << "):" << exp << std::endl;
+#else
+#  define LOG_VALUE(value)
+#  define LOG_NV(name, value)
+#  define DLOG(exp)
 #endif
+#include <sstream>
+#define ERR(exp)                                                       \
+  {                                                                    \
+    std::ostringstream oss;                                            \
+    oss << __FILE__ << ":" << __LINE__ << "(" << errno << "):" << exp; \
+    throw oss.str();                                                   \
+  }
