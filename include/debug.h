596f86ca141db5c56f34e13bcfc5ba05f85328e7
Aaron Robert <Robert.Aaron2017@outlook.com>
2020-05-02 17:20:51 +0800

    feat(debug): add print errno

--- include/debug.h
+++ include/debug.h
@@ -1,21 +1,24 @@
 #pragma once
+#include <errno.h>
+#include <string.h>
+#define PRINT_ERRNO errno << ": " << strerror(errno)
 #define DEBUG
 #ifdef DEBUG
 #  include <iostream>
 #  define LOG_VALUE(value) #  value << "=" << value << " "
 #  define LOG_NV(name, value) name << "=" << value << " "
 #  define DLOG(exp) \
-    std::cout << __FILE__ << ":" << __LINE__ << "(" << errno << "):" << exp << std::endl;
+    std::cout << __FILE__ << ":" << __LINE__ << "(" << PRINT_ERRNO << "):" << exp << std::endl;
 #else
 #  define LOG_VALUE(value)
 #  define LOG_NV(name, value)
 #  define DLOG(exp)
 #endif
 #include <sstream>
-#define ERR(exp)                        \
-  {                                     \
-    std::ostringstream oss;             \
-    oss << "(" << errno << "):" << exp; \
-    DLOG("Error: " << oss.str())        \
-    throw oss.str();                    \
+#define ERR(exp)                              \
+  {                                           \
+    std::ostringstream oss;                   \
+    oss << "(" << PRINT_ERRNO << "):" << exp; \
+    DLOG("Error: " << oss.str())              \
+    throw oss.str();                          \
   }
