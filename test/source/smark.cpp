24c2261dd7a8f83a14f8102358712b537e2517b0
Aaron Robert <Robert.Aaron2017@outlook.com>
2020-05-02 13:30:53 +0800

    test(tcp_client): add client test case

--- test/source/smark.cpp
+++ test/source/smark.cpp
@@ -1,6 +1,13 @@
+#include <client.h>
 #include <doctest/doctest.h>
 #include <pthread.h>
 #include <smark.h>
+
+#include <cstring>
+#include <future>
+
+#include "debug.h"
+#include "util.h"
 
 #if defined(_WIN32) || defined(WIN32)
 // apparently this is required to compile in MSVC++
@@ -30,9 +37,29 @@ void RunServer() {
   }
 }
 
-TEST_CASE("Client") {
+TEST_CASE("TCPClient") {
   RunServer();
-  pthread_join(svr_thread, nullptr);
+  int task = 0;
+  using namespace smark;
+  TCPClient cli;
+  util::EventLoop el(13);
+  el.SetEvent(&cli);
+  cli.Connect("127.0.0.1", 3000);
+  DLOG("Connected");
+  const char* data = "Hello world";
+  cli.writable_event = [&cli, &task, &data](util::EventLoop* el) {
+    (void)el;
+    task++;
+    cli.Send(data, sizeof(data));
+  };
+  cli.readable_event = [&cli, &task, &data](util::EventLoop* el) {
+    (void)el;
+    task++;
+    char buff[1024];
+    cli.Recv(buff, sizeof(buff));
+    CHECK(strcmp(buff, data) == 0);
+  };
+  el.Wait();
 }
 
 TEST_CASE("Smark") {
