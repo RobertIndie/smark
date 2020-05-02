85399f9b5fa907d4124dee84c237f15aca050430
Aaron Robert <Robert.Aaron2017@outlook.com>
2020-05-02 23:47:04 +0800

    style(ae): fix style

--- third_party/ae/include/zmalloc.h
+++ third_party/ae/include/zmalloc.h
@@ -36,40 +36,44 @@
 #define __str(s) #s
 
 #if defined(USE_TCMALLOC)
-#define ZMALLOC_LIB ("tcmalloc-" __xstr(TC_VERSION_MAJOR) "." __xstr(TC_VERSION_MINOR))
-#include <google/tcmalloc.h>
-#if (TC_VERSION_MAJOR == 1 && TC_VERSION_MINOR >= 6) || (TC_VERSION_MAJOR > 1)
-#define HAVE_MALLOC_SIZE 1
-#define zmalloc_size(p) tc_malloc_size(p)
-#else
-#error "Newer version of tcmalloc required"
-#endif
+#  define ZMALLOC_LIB \
+    ("tcmalloc-" __xstr(TC_VERSION_MAJOR) "." __xstr(TC_VERSION_MINOR))
+#  include <google/tcmalloc.h>
+#  if (TC_VERSION_MAJOR == 1 && TC_VERSION_MINOR >= 6) || (TC_VERSION_MAJOR > 1)
+#    define HAVE_MALLOC_SIZE 1
+#    define zmalloc_size(p) tc_malloc_size(p)
+#  else
+#    error "Newer version of tcmalloc required"
+#  endif
 
 #elif defined(USE_JEMALLOC)
-#define ZMALLOC_LIB ("jemalloc-" __xstr(JEMALLOC_VERSION_MAJOR) "." __xstr(JEMALLOC_VERSION_MINOR) "." __xstr(JEMALLOC_VERSION_BUGFIX))
-#include <jemalloc/jemalloc.h>
-#if (JEMALLOC_VERSION_MAJOR == 2 && JEMALLOC_VERSION_MINOR >= 1) || (JEMALLOC_VERSION_MAJOR > 2)
-#define HAVE_MALLOC_SIZE 1
-#define zmalloc_size(p) je_malloc_usable_size(p)
-#else
-#error "Newer version of jemalloc required"
-#endif
+#  define ZMALLOC_LIB                                       \
+    ("jemalloc-" __xstr(JEMALLOC_VERSION_MAJOR) "." __xstr( \
+        JEMALLOC_VERSION_MINOR) "." __xstr(JEMALLOC_VERSION_BUGFIX))
+#  include <jemalloc/jemalloc.h>
+#  if (JEMALLOC_VERSION_MAJOR == 2 && JEMALLOC_VERSION_MINOR >= 1) \
+      || (JEMALLOC_VERSION_MAJOR > 2)
+#    define HAVE_MALLOC_SIZE 1
+#    define zmalloc_size(p) je_malloc_usable_size(p)
+#  else
+#    error "Newer version of jemalloc required"
+#  endif
 
 #elif defined(__APPLE__)
-#include <malloc/malloc.h>
-#define HAVE_MALLOC_SIZE 1
-#define zmalloc_size(p) malloc_size(p)
+#  include <malloc/malloc.h>
+#  define HAVE_MALLOC_SIZE 1
+#  define zmalloc_size(p) malloc_size(p)
 #endif
 
 #ifndef ZMALLOC_LIB
-#define ZMALLOC_LIB "libc"
+#  define ZMALLOC_LIB "libc"
 #endif
 
 /* We can enable the Redis defrag capabilities only if we are using Jemalloc
  * and the version used is our special version modified for Redis having
  * the ability to return per-allocation fragmentation hints. */
 #if defined(USE_JEMALLOC) && defined(JEMALLOC_FRAG_HINT)
-#define HAVE_DEFRAG
+#  define HAVE_DEFRAG
 #endif
 
 void *zmalloc(size_t size);
