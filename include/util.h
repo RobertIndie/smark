a19ed887c5f09a0dd308b15535b4d8d5b19d2608
Aaron Robert <Robert.Aaron2017@outlook.com>
2020-06-16 23:44:02 +0800

    feat(util): enable shared_from_this in constructor

--- include/util.h
+++ include/util.h
@@ -20,6 +20,39 @@ extern "C" {
                                                           [&](void*) { X }};
 
 namespace smark::util {
+  // enabe shared_from_this in construct
+  // from http://www.cplusplus.com/forum/general/202645/
+  template <class T> void construct_deleter(T* t) {
+    t->~T();
+    free(t);
+  }
+
+  template <class T> class enable_shared_from_this {
+  public:
+    std::shared_ptr<T>* _construct_pself;
+    std::weak_ptr<T> _construct_self;
+
+    std::shared_ptr<T> shared_from_this() {
+      if (_construct_pself) {
+        return *_construct_pself;
+      } else {
+        return _construct_self.lock();
+      }
+    }
+  };
+
+  template <class T, typename... Params> std::shared_ptr<T> make_shared(Params&&... args) {
+    std::shared_ptr<T> rtn;
+    T* t = (T*)calloc(1, sizeof(T));
+    rtn.reset(t, construct_deleter<T>);
+    t->_construct_pself = &rtn;
+    t = new (t) T(std::forward<Params>(args)...);
+    t->_construct_pself = NULL;
+    t->_construct_self = rtn;
+
+    return rtn;
+  }
+
   typedef std::function<void(int)> CallbackType;  // void(int status)
   class EventLoop;
   class IEventObj {
