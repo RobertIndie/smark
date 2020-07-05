d58e62b1aae03e719bb0bb6486fbaaa811c64d48
Victor-wwj <wwj@.localdomain>
2020-07-05 13:43:37 +0800

    delete _name member ,greet function

--- source/smark.cpp
+++ source/smark.cpp
@@ -7,21 +7,6 @@
 #include "util.h"
 using namespace smark;
 
-Smark::Smark(std::string _name) : name(_name) {}
-
-std::string Smark::greet(LanguageCode lang) const {
-  switch (lang) {
-    default:
-    case LanguageCode::EN:
-      return "Hello, " + name + "!";
-    case LanguageCode::DE:
-      return "Hallo " + name + "!";
-    case LanguageCode::ES:
-      return "¡Hola " + name + "!";
-    case LanguageCode::FR:
-      return "Bonjour " + name + "!";
-  }
-}
 
 namespace smark {
   void Smark::Run() {
